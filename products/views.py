46ef413be260c23df6cbb9a0ea53dfeac9809cdf
Kliment-Fis <kirilltrun2015@gmail.com>
2022-08-06 23:48:13 +0300

    update app products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(requests):
+    return render(requests, "products/index.html")
+
+
+def products(requests):
+    return render(requests, "products/products.html")
